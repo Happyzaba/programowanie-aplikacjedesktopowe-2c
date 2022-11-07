4b3dd84fbb7363d4f74596a01529a09fbb6d8e6c
me <f@wp.pl>
2022-11-07 09:54:17 +0100

    cSharp

---
+++ Programowanie strukturalne i obiektowe/FirstObjectConsoleApp/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstObjectConsoleApp
+{
+    class Person
+    {
+
+       private string name;
+       private string surname;
+       private int age;
+
+        public Person()
+        {
+            name = "Bezimienny";
+            surname = "Nieznany";
+            age = 21;
+
+        }
+        public Person(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+
+
+
+
+
+    }
+}
