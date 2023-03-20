7d8fb04467f07cd095ba8f8751d0ea18c62bfd59
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-03-20 10:40:03 +0100

    trsyhfkhv

---
+++ Programowanie strukturalne i obiektowe/SchoolAPP/Database/SchoolDatabase.cs
@@ -0,0 +1,14 @@
+﻿using SchoolAPP.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAPP.Database
+{
+    class SchoolDatabase
+    {
+        public List<SchoolClass> SchoolClasses { get; set; } = new List<SchoolClass>();
+    }
+}
---
+++ Programowanie strukturalne i obiektowe/SchoolAPP/Program.cs
@@ -0,0 +1,45 @@
+﻿using SchoolAPP.Database;
+using SchoolAPP.Database.Entities;
+using System;
+
+namespace SchoolAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolDatabase schoolDatabase = new SchoolDatabase();
+            SchoolClass schoolClass = new SchoolClass();
+            schoolClass.Id = 1;
+            schoolClass.Name = "1A";
+
+            schoolDatabase.SchoolClasses.Add(schoolClass);
+
+            schoolClass = new SchoolClass(){
+                Id = 2,
+                Name = "1B"
+            };
+            schoolDatabase.SchoolClasses.Add(schoolClass);
+
+            schoolClass = new SchoolClass()
+            {
+                Id = 3,
+                Name = "1C"
+            };
+            schoolDatabase.SchoolClasses.Add(schoolClass);
+
+            schoolClass = new SchoolClass()
+            {
+                Id = 4,
+                Name = "2C"
+            };
+            schoolDatabase.SchoolClasses.Add(schoolClass);
+
+            Console.WriteLine("Lista klas w szkole: ");
+            foreach (var item in schoolDatabase.SchoolClasses)
+            {
+                Console.WriteLine(item.Id + " " + item.Name);
+            }
+        }
+    }
+}
