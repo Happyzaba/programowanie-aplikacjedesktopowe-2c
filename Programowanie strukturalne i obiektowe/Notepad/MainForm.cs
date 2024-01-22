e25abc8ebb54eade0acf668bcacfdcedf8b069d7
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-22 13:16:53 +0100

    dodanie

--- Programowanie strukturalne i obiektowe/Notepad/MainForm.cs
+++ Programowanie strukturalne i obiektowe/Notepad/MainForm.cs
@@ -4,6 +4,8 @@ namespace Notepad
 {
     public partial class MainForm : Form
     {
+        string filePath = "";
+
         public MainForm()
         {
             InitializeComponent();
@@ -17,6 +19,43 @@ namespace Notepad
         private void nowyToolStripMenuItem_Click(object sender, EventArgs e)
         {
             textBoxNotepad.Text = "";
+            filePath = "";
+        }
+
+        private void otwórzToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                textBoxNotepad.Text = File.ReadAllText(openFileDialog.FileName);
+                filePath = openFileDialog.FileName;
+            }
+        }
+
+        private void zapiszJakoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBoxNotepad.Text);
+                filePath = saveFileDialog.FileName;
+            }
+        }
+
+        private void zapiszToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (filePath != "" ||
+                saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (filePath == "")
+                    filePath = saveFileDialog.FileName;
+                File.WriteAllText(filePath, textBoxNotepad.Text);
+
+            }
+        }
+
+        private void cofnijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBoxNotepad.Undo();
         }
     }
 }
