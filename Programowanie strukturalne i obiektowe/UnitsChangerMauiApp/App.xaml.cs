7b9c0dd90825d95d0f004e62a04a674c63cdbe44
q <q>
2024-10-01 14:22:05 +0200

    zadanie

---
+++ Programowanie strukturalne i obiektowe/UnitsChangerMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitsChangerMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Programowanie strukturalne i obiektowe/UnitsChangerMauiApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+
+namespace UnitsChangerMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<string> UnitsList { get; set; }
+        public string FirstSelectedUnit { get; set; }
+        public string SecondSelectedUnit { get; set; }
+        public string ChangedUnit { get; set; }
+
+        private string changedUnitMessage;
+
+        public string ChangedUnitMessage
+        {
+            get { return changedUnitMessage; }
+            set { 
+                changedUnitMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainPage()
+        {
+            UnitsList = new List<string>();
+            UnitsList.Add("mm");
+            UnitsList.Add("cm");
+            UnitsList.Add("m");
+            UnitsList.Add("km");
+
+            FirstSelectedUnit = UnitsList.First();
+            SecondSelectedUnit = UnitsList.First();
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+
+            ChangedUnitMessage = " Liczba po zamianie " + SecondSelectedUnit;
+        }
+    }
+
+}
