f8464af78db4c3b21252e4838eb1ec37e23e3030
q <q>
2024-12-06 15:00:33 +0100

    egzamin

---
+++ Programowanie strukturalne i obiektowe/EgzaminMauiApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace EgzaminMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
