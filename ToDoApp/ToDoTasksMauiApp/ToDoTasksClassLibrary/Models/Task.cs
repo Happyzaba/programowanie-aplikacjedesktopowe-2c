e42e60e6f0775df0aa3698a2dd4af30355ddee7a
q <q>
2025-05-13 11:05:31 +0200

    bazy

---
+++ ToDoApp/ToDoTasksMauiApp/ToDoTasksClassLibrary/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoTasksClassLibrary.Models;
+
+public partial class Task
+{
+    public int Id { get; set; }
+
+    public string Content { get; set; } = null!;
+
+    public bool IsDone { get; set; }
+
+    public bool IsDeleted { get; set; }
+}
