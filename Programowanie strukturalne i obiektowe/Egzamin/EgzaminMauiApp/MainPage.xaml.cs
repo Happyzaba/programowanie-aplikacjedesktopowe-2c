d7163660c5ffb817970d913ca46af0abe6730353
Sala213 <Sala213@ezse.pl>
2025-09-18 14:26:25 +0200

    egzamin

---
+++ Programowanie strukturalne i obiektowe/Egzamin/EgzaminMauiApp/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EgzaminMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Programowanie strukturalne i obiektowe/Zad1ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        Console.WriteLine("Ile wygerenować losowań?");
+        int losowania = int.Parse(Console.ReadLine());
+        Console.WriteLine("Zestawy wylosowanych liczb");
+
+        for (int i = 1; i <= losowania; i++) {
+            Console.WriteLine("Losowanie: " + i);
+            
+
+        }
+    }
+}
