b30bcac9686deb5d14674305ec990f6f62ccd64f
Sala213 <Sala213@ezse.pl>
2025-09-18 12:37:51 +0200

    maui

---
+++ Programowanie strukturalne i obiektowe/TabPageMauiApp/Another.xaml.cs
@@ -0,0 +1,9 @@
+namespace TabPageMauiApp;
+
+public partial class Another : ContentPage
+{
+	public Another()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Programowanie strukturalne i obiektowe/TabPageMauiApp/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.Services.Maps;
+
+namespace TabPageMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            TabbedPage tabbedPage = new TabbedPage();
+
+            tabbedPage.Children.Add(new MainPage());
+            tabbedPage.Children.Add(new AboutPage());
+            tabbedPage.Children.Add(new Another());
+
+            return new Window(tabbedPage);
+        }
+    }
+}
